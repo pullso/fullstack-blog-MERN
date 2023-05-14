a685fb0c10f999a0b43658d515f6eaeaf45e751f
pullso <pullso.code@gmail.com>
2023-05-14 18:48:44 +0300

    add new functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
 import Container from "@mui/material/Container";
 
-import { Header } from "./components";
-import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import {Header} from "./components";
+import {Route, Routes} from "react-router-dom";
+import {Home, FullPost, Registration, AddPost, Login} from "./pages";
+import {useEffect} from "react";
+import {fetchAuthMe, selectIsAuth} from "./redux/slices/auth";
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  const dispatch = useDispatch()
+  const isAuth = useSelector(selectIsAuth)
+
+  useEffect(() => {
+    dispatch(fetchAuthMe())
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header/>
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/posts/:id" element={<FullPost />}/>
+          <Route path="/add-post" element={<AddPost />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Registration />}/>
+        </Routes>
       </Container>
     </>
   );
